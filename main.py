d67ced08c01ba6745100b8c2c7e16fa5fba1c796
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-09-15 21:08:15 +0530

    calendar

---
+++ main.py
@@ -0,0 +1,8 @@
+import math
+print("Floor and ceiling value of 23.56 are:" + str(math.ceil(23.56)) +',' + str(math.floor(23.56)))
+
+x = 10
+y =-15
+print("The value of x after copying the sign from y is:" + str(math.copysign(x,y)))
+
+print("The absolute valu of -96 and 56 are", + str(math.fabs(-96)) + "," + str(math.fabs(56))) 
---
+++ main5.py
@@ -0,0 +1,61 @@
+print("2013 CALENDAR")
+import calendar
+
+yy = 2013
+mm = 1
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 2
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 3
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 4
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 5
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 6
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 7
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 8
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 9
+print(calendar.month(yy, mm))
+
+yy = 2013
+mm = 10
+print(calendar.month(yy, mm))
+
+
+
+yy = 2013
+mm = 11
+print(calendar.month(yy, mm))
+
+
+yy = 2013
+mm = 12
+print(calendar.month(yy, mm))
